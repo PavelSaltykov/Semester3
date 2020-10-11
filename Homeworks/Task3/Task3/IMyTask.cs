3fbb4ae71301e64a13cf9352582a432db40eb90e
Pavel <pavsaltykov@mail.ru>
2020-10-11 20:42:50 +0300

    added interface IMyTask

---
+++ Homeworks/Task3/Task3/IMyTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task3
+{
+    public interface IMyTask<TResult>
+    {
+        bool IsCompleted { get; }
+
+        TResult Result { get; }
+
+        IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> func);
+    }
+}
---
+++ Homeworks/Task3/Task3/MyThreadPool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task3
+{
+    public class MyThreadPool
+    {
+        public MyThreadPool(int numberOfThreads)
+        {
+            if (numberOfThreads <= 0)
+                throw new ArgumentException("Number of threads must be greater than 0.");
+
+        }
+
+
+    }
+}
