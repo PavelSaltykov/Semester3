f472fbb903c4f0755062e11d879c7b815b00805d
Pavel <pavsaltykov@mail.ru>
2020-10-12 01:34:49 +0300

    implemented MyTask without ContinueWith

--- Homeworks/Task3/Task3/MyThreadPool.cs
+++ Homeworks/Task3/Task3/MyThreadPool.cs
@@ -1,16 +1,68 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
 
 namespace Task3
 {
     public class MyThreadPool
     {
+        private class MyTask<TResult> : IMyTask<TResult>
+        {
+            public MyTask(Func<TResult> supplier) => this.supplier = supplier;
+
+            private Func<TResult> supplier;
+            public bool IsCompleted { get; private set; }
+            private Exception caughtException;
+            private readonly ManualResetEvent ManualResetEvent = new ManualResetEvent(false);
+
+            private TResult result;
+            public TResult Result
+            {
+                get
+                {
+                    ManualResetEvent.WaitOne();
+
+                    if (caughtException != null)
+                        throw new AggregateException(caughtException);
+
+                    return result;
+                }
+            }
+
+            public void Run()
+            {
+                try
+                {
+                    result = supplier();
+                }
+                catch (Exception e)
+                {
+                    caughtException = e;
+                }
+                finally
+                {
+                    supplier = null;
+                    IsCompleted = true;
+                    ManualResetEvent.Set();
+                }
+            }
+
+            public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> func)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private ConcurrentQueue<Action> actionQueue;
+        private Thread[] threads;
+        private AutoResetEvent AutoResetEvent = new AutoResetEvent(false);
+
         public MyThreadPool(int numberOfThreads)
         {
             if (numberOfThreads <= 0)
                 throw new ArgumentException("Number of threads must be greater than 0.");
 
         }
-
 
     }
 }
