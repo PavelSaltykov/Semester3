3f480db2c9152b1cc359209b390f0ededf8c6d5f
Pavel <pavsaltykov@mail.ru>
2020-11-13 17:26:20 +0300

    added TestResult class

---
+++ Homeworks/Task5/Task5/TestResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task5
+{
+    public class TestResult
+    {
+        public string AssemblyName { get; }
+
+        public string MethodName { get; }
+
+        public bool IsPassed { get; }
+
+        public bool Ignored { get; }
+
+        public string ReasonToIgnore { get; }
+
+        public TimeSpan Time { get; }
+
+        public TestResult(string assemblyName, string methodName, 
+            bool isPassed, bool ignored, string reason, TimeSpan time)
+        {
+            AssemblyName = assemblyName;
+            MethodName = methodName;
+            IsPassed = isPassed;
+            Ignored = ignored;
+            ReasonToIgnore = reason;
+            Time = time;
+        }
+    }
+}
