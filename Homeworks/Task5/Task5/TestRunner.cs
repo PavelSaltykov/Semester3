a2321341e3fe0b3c3b1954401177f81bf282e4cd
Pavel <pavsaltykov@mail.ru>
2020-11-16 16:54:21 +0300

    implemented test runner

--- Homeworks/Task5/Task5/TestRunner.cs
+++ Homeworks/Task5/Task5/TestRunner.cs
@@ -1,24 +1,124 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Task5.Attributes;
 using Task5.TestInformation;
 
 namespace Task5
 {
     public class TestRunner
     {
+        private readonly ConcurrentQueue<TestInfo> testsInfo = new ConcurrentQueue<TestInfo>();
+
+        private readonly ConcurrentQueue<Type> classTypes;
+
         public TestRunner(string path)
         {
+            var assemblyFiles = Directory.EnumerateFiles(path, "*.dll", SearchOption.AllDirectories);
+            if (assemblyFiles.Count() == 0)
+                throw new InvalidOperationException($"Assemblies not found in path: {path}.");
 
+            var classes = assemblyFiles.Select(Assembly.Load)
+                .SelectMany(a => a.ExportedTypes)
+                .Where(t => t.IsClass);
+
+            var classesContaininigTests = classes.Where(c => c.GetMethods()
+                .Any(mi => mi.GetCustomAttributes().Any(attr => attr is TestAttribute)));
+
+            classTypes = new ConcurrentQueue<Type>(classesContaininigTests);
         }
 
-        public void Run()
+        public void Run() => Parallel.ForEach(classTypes, ExecuteTestsInClass);
+
+        private void ExecuteTestsInClass(Type classType)
         {
-
+            ExecuteStaticMethods(classType, typeof(BeforeClassAttribute));
+            ExecuteTestMethods(classType);
+            ExecuteStaticMethods(classType, typeof(AfterClassAttribute));
         }
 
-        public IEnumerable<TestInfo> GetTestsInfo()
+        private void ExecuteStaticMethods(Type classType, Type attributeType)
         {
-            throw new NotImplementedException();
+            var methods = GetMethodsWithAttribute(classType, attributeType);
+
+            foreach (var method in methods)
+            {
+                if (!method.IsStatic)
+                    throw new InvalidOperationException($"{method} must be static.");
+
+                method.Invoke(null, null);
+            }
         }
+
+        private void ExecuteTestMethods(Type classType)
+        {
+            var instance = Activator.CreateInstance(classType);
+
+            var beforeMethods = GetMethodsWithAttribute(classType, typeof(BeforeAttribute));
+            var afterMethods = GetMethodsWithAttribute(classType, typeof(AfterAttribute));
+
+            var testMethods = GetMethodsWithAttribute(classType, typeof(TestAttribute));
+            foreach (var test in testMethods)
+            {
+                foreach (var method in beforeMethods)
+                {
+                    method.Invoke(instance, null);
+                }
+                ExecuteTest(test, instance);
+                foreach (var method in afterMethods)
+                {
+                    method.Invoke(instance, null);
+                }
+            }
+        }
+
+        private void ExecuteTest(MethodInfo method, object instance)
+        {
+            var assemblyName = method.DeclaringType.Assembly.GetName().Name;
+            var className = method.DeclaringType.Name;
+            var methodName = method.Name;
+
+            var attribute = (TestAttribute)method.GetCustomAttribute(typeof(TestAttribute));
+            if (attribute.Ignore != null)
+            {
+                var testInfo = new IgnoredTestInfo(assemblyName, className, methodName, attribute.Ignore);
+                testsInfo.Enqueue(testInfo);
+                return;
+            }
+
+            var stopwatch = new Stopwatch();
+            var isPassed = false;
+            Exception unexpected = null;
+            try
+            {
+                stopwatch.Start();
+                method.Invoke(instance, null);
+                isPassed = attribute.Expected == null;
+            }
+            catch (Exception e) when (e.GetType() != attribute.Expected)
+            {
+                unexpected = e;
+            }
+            finally
+            {
+                stopwatch.Stop();
+                var testInfo = new TestResultInfo(assemblyName, className, methodName, 
+                    isPassed, unexpected, stopwatch.Elapsed);
+                testsInfo.Enqueue(testInfo);
+            }
+        }
+
+        private IEnumerable<MethodInfo> GetMethodsWithAttribute(Type classType, Type attributeType)
+        {
+            return classType.GetMethods()
+                .Where(mi => mi.GetCustomAttributes().Any(attr => attr.GetType() == attributeType));
+        }
+
+        public IEnumerable<TestInfo> GetTestsInfo() => testsInfo;
     }
 }
