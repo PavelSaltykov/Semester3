095afca1932bff991622aaaf061c2f5023864db1
Pavel <pavsaltykov@mail.ru>
2020-12-08 11:24:34 +0300

    added test2

---
+++ Tests/Test2/Test2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Test2
+{
+    public class Program
+    {
+        private static string ByteArrayToString(byte[] ba) => BitConverter.ToString(ba).Replace("-", "");
+
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Incorrect application arguments.");
+                return;
+            }
+
+            var path = args[0];
+            Console.WriteLine($"Path: {path}");
+            Console.WriteLine("Check-sum: ");
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var singleThreadedRes = CheckSum.ComputeSingleThreaded(path);
+            stopwatch.Stop();
+
+            Console.WriteLine($"Single-threaded result: {ByteArrayToString(singleThreadedRes)}");
+            Console.WriteLine($"Time: {stopwatch.Elapsed}");
+            Console.WriteLine();
+
+            stopwatch = Stopwatch.StartNew();
+            var multiThreadedRes = CheckSum.ComputeMultiThreaded(path).Result;
+            stopwatch.Stop();
+
+            Console.WriteLine($"Multi-threaded result: {ByteArrayToString(multiThreadedRes)}");
+            Console.WriteLine($"Time: {stopwatch.Elapsed}");
+        }
+    }
+}
---
+++ Tests/Test2/Test2Tests/CheckSumTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+
+namespace Test2.Tests
+{
+    [TestFixture]
+    public class CheckSumTests
+    {
+        [TestCase(@"..\..\..\..")]
+        [TestCase(@"..\..\..")]
+        public void ComputeSingleThreadedTest(string path)
+        {
+            var checkSum1 = CheckSum.ComputeSingleThreaded(path);
+            var checkSum2 = CheckSum.ComputeSingleThreaded(path);
+            Assert.AreEqual(checkSum1, checkSum2);
+        }
+
+        [TestCase(@"..\..\..\..")]
+        [TestCase(@"..\..\..")]
+        public void ComputeMultiThreadedTest(string path)
+        {
+            var checkSum1 = CheckSum.ComputeMultiThreaded(path).Result;
+            var checkSum2 = CheckSum.ComputeMultiThreaded(path).Result;
+            Assert.AreEqual(checkSum1, checkSum2);
+        }
+
+        [Test]
+        public void DifferentPathsSingleThreadedTest()
+        {
+            var checkSum1 = CheckSum.ComputeSingleThreaded(@"..\..");
+            var checkSum2 = CheckSum.ComputeSingleThreaded(".");
+            Assert.AreNotEqual(checkSum1, checkSum2);
+        }
+
+        [Test]
+        public void DifferentPathsMultiThreadedTest()
+        {
+            var checkSum1 = CheckSum.ComputeMultiThreaded(@"..\..").Result;
+            var checkSum2 = CheckSum.ComputeMultiThreaded(".").Result;
+            Assert.AreNotEqual(checkSum1, checkSum2);
+        }
+
+        [Test]
+        public void SingleAndMultiThreadedTest()
+        {
+            const string path = @"..\..\..\..";
+            var checkSum1 = CheckSum.ComputeSingleThreaded(path);
+            var checkSum2 = CheckSum.ComputeMultiThreaded(path).Result;
+            Assert.AreEqual(checkSum1, checkSum2);
+        }
+    }
+}
