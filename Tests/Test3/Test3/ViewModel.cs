8714b50c8557042d0d1cdcb8831b00f70ac50688
Pavel <pavsaltykov@mail.ru>
2020-12-15 11:30:35 +0300

    added test3

---
+++ Tests/Test3/Test3/ViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+
+namespace Test3
+{
+    public class ViewModel
+    {
+        public ObservableCollection<char> Board { get; set; } = new ObservableCollection<char>();
+
+        public ViewModel() => Initialize();
+
+        private void Initialize()
+        {
+            Board.Clear();
+            for (var i = 0; i < 9; ++i)
+            {
+                Board.Add(' ');
+            }
+        }
+
+        private bool firstPlayer = true;
+        private MakeMoveCommand makeMoveCommand;
+        public MakeMoveCommand MakeMoveCommand
+        {
+            get
+            {
+                return makeMoveCommand ??= new MakeMoveCommand(index => MakeMove(index),
+                    index => Board[index] == ' ');
+            }
+        }
+
+        private void MakeMove(int index)
+        {
+            Board[index] = firstPlayer ? 'X' : 'O';
+            firstPlayer = !firstPlayer;
+
+            if (WinnerChecker.Check(Board.ToArray()) == -1)
+                return;
+
+            firstPlayer = true;
+            if (WinnerChecker.Check(Board.ToArray()) == 0)
+            {
+                MessageBox.Show("Draw.");
+                Initialize();
+                return;
+            }
+
+            var number = WinnerChecker.Check(Board.ToArray()) == 1 ? "First" : "Second";
+            MessageBox.Show($"{number} player wins!");
+            Initialize();
+        }
+    }
+}
---
+++ Tests/Test3/Test3/WinnerChecker.cs
@@ -0,0 +1,34 @@
+﻿namespace Test3
+{
+    public static class WinnerChecker
+    {
+        public static int Check(char[] field)
+        {
+            return -1;
+        }
+
+        private static int CheckRow(char[] field, int rowNumber)
+        {
+            int firstPlayerCount = 0;
+            int secondPlayerCount = 0;
+            for (var i = rowNumber; i < 3; ++i)
+            {
+                if (field[rowNumber * 3 + i] == 'X')
+                {
+                    firstPlayerCount++;
+                }
+                if(field[rowNumber * 3 + i] == 'O')
+                {
+                    secondPlayerCount++;
+                }
+            }
+
+            if (firstPlayerCount == 3)
+                return 1;
+            if (secondPlayerCount == 3)
+                return 2;
+
+            return -1;
+        }
+    }
+}
