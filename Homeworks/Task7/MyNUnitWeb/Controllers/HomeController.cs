8116a63f05553730554c35f751c56df7fa22e355
Pavel <pavsaltykov@mail.ru>
2021-01-09 14:48:10 +0300

    implemented uploading assemblies

--- Homeworks/Task7/MyNUnitWeb/Controllers/HomeController.cs
+++ Homeworks/Task7/MyNUnitWeb/Controllers/HomeController.cs
@@ -1,18 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyNUnitWeb.Models;
 using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 using Task7.Models;
 
 namespace Task7.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController()
+        private readonly IWebHostEnvironment environment;
+        private string PathToAssemblies => Path.Combine(environment.WebRootPath, "Assemblies");
+
+        public HomeController(IWebHostEnvironment environment)
         {
+            this.environment = environment;
+            Directory.CreateDirectory(PathToAssemblies);
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var ivm = new IndexViewModel(PathToAssemblies);
+            return View(ivm);
+        }
+
+        [HttpPost]
+        public IActionResult Clear()
+        {
+            var files = Directory.GetFiles(PathToAssemblies);
+            foreach (var file in files)
+            {
+                System.IO.File.Delete(file);
+            }
+            var ivm = new IndexViewModel(PathToAssemblies);
+            return RedirectToAction("Index", ivm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadAssemblies(IFormFileCollection files)
+        {
+            foreach (var file in files)
+            {
+                var path = Path.Combine(PathToAssemblies, file.FileName);
+                await using var fileStream = System.IO.File.Create(path);
+                await file.CopyToAsync(fileStream);
+            }
+            var ivm = new IndexViewModel(PathToAssemblies);
+            return RedirectToAction("Index", ivm);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
